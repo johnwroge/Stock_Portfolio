0d02b2fea7bf8b710e9551cd1f4a66a502522561
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-08-15 11:00:06 -0400

    Updated responsive styles for mobile

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <h1 className="nav">Manrico's Stock Portfolio </h1>
+    <nav className="navbar">
+      <h1 className="nav-title">Manrico's Stock Portfolio</h1>
     </nav>
   );
 };
